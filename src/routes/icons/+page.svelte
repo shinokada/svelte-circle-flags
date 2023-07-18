ccd375015ae5c6fa0bc5cc4eebd52c544d8a9240
Shinichi Okada <okada.shin@gmail.com>
2023-07-18 10:47:00 +0200

    fix: update flowbite-svelte v0.40.1, add @floating-ui/dom remove flowbite and popper, add tests

--- src/routes/icons/+page.svelte
+++ src/routes/icons/+page.svelte
@@ -1,23 +1,10 @@
 <script>
   import Label from 'flowbite-svelte/Label.svelte';
   import Range from 'flowbite-svelte/Range.svelte';
-  import Tabs from 'flowbite-svelte/Tabs.svelte';
-  import TabItem from 'flowbite-svelte/TabItem.svelte';
   import TableSearch from 'flowbite-svelte/TableSearch.svelte';
 
   import * as Icons from '$lib';
 
-  const random_tailwind_color = () => {
-    const colors = ['red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink'];
-    const shades = ['300', '400', '500'];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    const randomShade = shades[Math.floor(Math.random() * shades.length)];
-    return `text-${randomColor}-${randomShade} dark:text-${randomColor}-${randomShade} shrink-0`;
-  };
-  const random_hex_color_code = () => {
-    let n = (Math.random() * 0xfffff * 1000000).toString(16);
-    return '#' + n.slice(0, 6);
-  };
   const contentClass = 'rounded-lg dark:bg-zinc-700 mt-4';
   let searchTerm = '';
 
@@ -26,7 +13,7 @@
   });
   let size= "24";
 </script>
-
+<h1>Svelte Circle Flags: Icons</h1>
 <TableSearch
   placeholder="Search by icon name"
   hoverable={true}
